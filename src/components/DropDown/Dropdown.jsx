b4aedc7dffbfa5356c260bf002c6045a3b9d1e6e
jeronimo fernandez <jeronimo2000.jf@gmail.com>
2022-11-07 17:13:39 +0100

    terminado casi

--- src/components/DropDown/Dropdown.jsx
+++ src/components/DropDown/Dropdown.jsx
@@ -7,111 +7,27 @@ import InfoGeneral from './InfoGeneral/InfoGeneral';
 import { CV } from '../../Data/CV/cv';
 
 const Dropdown = () => {
-const [showInfo, setShowInfo]= useState(true);
-const [showexperience, setShowExperience]= useState(false);
-const [showAcademico, setShowAcademico]= useState(false);
-const [showHabilidades, setShowHabilidades]= useState(false);
+const [show, setShow]= useState('Información General');
 
-function Funcion(){
-    const $$select = document.getElementsByClassName('select')
-    const $$valor= $$select[0].options[$$select[0].selectedIndex].value
-    
-    
-    if($$valor==='Información General'){
-        if(!!showHabilidades){
-            setShowHabilidades(!showHabilidades)
-            setShowInfo(!showInfo)
-        }
-        if(!!showAcademico){
-            setShowAcademico(!showAcademico)
-            setShowInfo(!showInfo)
-        }
-        if(!!showexperience){
-            setShowExperience(!showexperience)
-            setShowInfo(!showInfo)
-        } 
-        else{
-            setShowExperience(!!showInfo)
-        }
-        
-    } else if($$valor==='Experiencia Laboral'){
-        if(!!showInfo){
-            setShowInfo(!showInfo)
-            setShowExperience(!showexperience)
-        }
-        if(!!showHabilidades){
-            setShowHabilidades(!showHabilidades)
-            setShowExperience(!showexperience)
-            console.log($$valor)
-        }
-        if(!!showAcademico){
-            setShowAcademico(!showAcademico)
-            setShowExperience(!showexperience)
-            console.log($$valor)
-        } 
-        else {
-            setShowExperience(!showexperience)
-        }
-        
-        
-    } else if($$valor==='Habilidades'){
-        if(showInfo === true){
-            setShowInfo(!showInfo)
-            setShowHabilidades(!showHabilidades)
-        }
-        if(!!showAcademico){
-            setShowAcademico(!showAcademico)
-            setShowHabilidades(!showHabilidades)
-        }
-        if(!!showexperience){
-            
-            setShowExperience(!showexperience)
-            setShowHabilidades(!showHabilidades)
-        } else{
-            setShowHabilidades(!showHabilidades)
-        }
-        
-    } else if($$valor==='Historial Academico'){
-        if(!!showHabilidades){
-            setShowHabilidades(!showHabilidades)
-            setShowAcademico(!showAcademico)
-        }
-        if(showInfo === true){
-            setShowInfo(!showInfo)
-            setShowAcademico(!showAcademico)
-        }
-        if(!!showexperience){
-            setShowExperience(!showexperience)
-            setShowAcademico(!showAcademico)
-        } else{
-            setShowAcademico(!showAcademico)
-        }
-        
-    }
-}
-  return (
+
+    return (
         <div className='Dropdown'>
-            <select className='select' onChange={(e) => Funcion()}>
+            <select className='select' onChange={(e) => setShow(e.target.value)}>
                 <option value='Información General'>Información General</option>
                 <option value='Experiencia Laboral'>Experiencia Laboral</option>
                 <option value='Historial Academico'>Historial Academico</option>
                 <option value='Habilidades'>Habilidades</option>
             </select>
             <div className='DropInner'>
-            {showInfo ?
-            <InfoGeneral/>
-            : <></>}
-            {showexperience ?
-            <Experiencia data={CV}/>
-            : <></>}
-            {showAcademico ?
-            <Academico data={CV}/>
-            : <></>}
-            {showHabilidades ?
-            <Habilidades data={CV}/>
-            : <></>}
+            {show === 'Información General' &&
+            <InfoGeneral/>}
+            {show === 'Experiencia Laboral' &&
+            <Experiencia/>}
+            {show === 'Historial Academico' &&
+            <Academico/>}
+            {show === 'Habilidades' &&
+            <Habilidades/>}
             </div>
-            
         </div>
     )
 }
